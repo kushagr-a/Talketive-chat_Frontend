addde18fd3e25787f042781eb68ff72d20292157
Kushagra bharti <kushagrabharti16@gmail.com>
2025-06-30 22:14:33 +0530

    update cors error FF

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development"
-    ? "https://talketive-chatbackend-production.up.railway.app/api"
+    ? "http://localhost:5001/api"
     : "/api"),
   withCredentials: true,
 });
